f166fe296aab64d895fcc977771e7c4a9306e131
saccharin01 <chousik01@gmail.com>
2025-04-09 11:21:44 +0900

    new Module | QueryDSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,13 @@ repositories {
 }
 
 dependencies {
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+
+    // QueryDSL APT (Q클래스 생성용)
+    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api:3.1.0")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api:2.1.1")
+
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -37,4 +44,15 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+val querydslGeneratedDir = file("build/generated/querydsl")
+
+sourceSets {
+    named("main") {
+        java.srcDir(querydslGeneratedDir)
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.generatedSourceOutputDirectory.set(querydslGeneratedDir)
 }
