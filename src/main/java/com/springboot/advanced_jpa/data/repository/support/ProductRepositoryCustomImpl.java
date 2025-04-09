c9b35cfc51d99f9dfae8444b75d2127a4a36d27b
saccharin01 <chousik01@gmail.com>
2025-04-09 14:29:36 +0900

    new | ProductRepositoryCustomImpl.java
    인터페이스 ProductRepositoryCustom의 구현체 스켈레톤.
    
    jpa 에서 제공하는 메서드 외에 QueryDSL 에서 제공하는 기능을 활용하여 findByName 메서드를 정의한다.

---
+++ src/main/java/com/springboot/advanced_jpa/data/repository/support/ProductRepositoryCustomImpl.java
@@ -0,0 +1,18 @@
+package com.springboot.advanced_jpa.data.repository.support;
+
+import com.springboot.advanced_jpa.data.entity.Product;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class ProductRepositoryCustomImpl extends QuerydslRepositorySupport implements ProductRepositoryCustom {
+
+    public ProductRepositoryCustomImpl() {
+        super(Product.class);
+    }
+
+    @Override
+    public List<Product> findByName(String name){
+        return null;
+    }
+}
