d31d5e7052ded9994d72f323e449c4a4ce7c572d
saccharin01 <chousik01@gmail.com>
2025-04-16 18:48:41 +0900

    feat | ProductResponseDTO.java
    response로 돌아가는 객체에 데이터필드 추가.
    create_at
    update_at

--- src/main/java/com/springboot/advanced_jpa/data/dto/ProductResponseDTO.java
+++ src/main/java/com/springboot/advanced_jpa/data/dto/ProductResponseDTO.java
@@ -4,6 +4,9 @@ package com.springboot.advanced_jpa.data.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Getter
 @Setter
 public class ProductResponseDTO {
@@ -11,13 +14,17 @@ public class ProductResponseDTO {
     private String name;
     private int price;
     private int stock;
+    private LocalDateTime create_at;
+    private LocalDateTime update_at;
 
     public ProductResponseDTO() {}
 
-    public ProductResponseDTO(Long number, String name, int price, int stock) {
+    public ProductResponseDTO(Long number, String name, int price, int stock, LocalDateTime create_at, LocalDateTime update_at) {
         this.number = number;
         this.name = name;
         this.price = price;
         this.stock = stock;
+        this.create_at = create_at;
+        this.update_at = update_at;
     }
 }
